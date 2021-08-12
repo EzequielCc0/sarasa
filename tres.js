51d843fd87e9c784d365d67de14c9a829628289e
ezequielcc0 <ezeqieelbp@gmail.com>
2021-08-11 21:44:12 -0300

    poner un msj

--- tres.js
+++ tres.js
@@ -1,3 +1,77 @@
 function mostrar() {
-  alert("Ejercicio 3");
+  let tipo;
+  let origen;
+  let precio;
+  let respuesta;
+  let contadorMuneca=0;
+  let contadorRompecabezas=0;
+  let contadorPelota=0;
+  let jugueteMasVendido;
+  let contadorJuguetes=0;
+  let sumaPrecioJuguetes=0;
+  let promedioJuguetes=0;
+  let recaudacionTotal=0;
+  let precioMasBarato=5001;
+  let IVA=0;
+  let totalDeTodo=0;
+  do{
+    tipo=prompt("Inserte tipo de jueguete (muñeca/rompecabezas/pelota):").toLowerCase();
+  while(!(tipo == "muñeca"||tipo == "rompecabezas"||tipo == "pelota")){
+    tipo=prompt("ERROR. Inserte tipo de jueguete (muñeca/rompecabezas/pelota):").toLowerCase();
+  }
+  origen=prompt("Inserte origen (nacional/importado):").toLowerCase();
+  while (!(origen == "nacional"||origen == "importado")) {
+    origen=prompt("ERROR. Inserte origen (nacional/importado):").toLowerCase();
+  }
+  precio=parseInt(prompt("Inserte precio (entre $1000 y $5000):"));
+  while (isNaN(precio)||precio<1000||precio>5000) {
+    precio=parseInt(prompt("ERROR. Inserte precio(entre $1000 y $5000):"));
+  }
+  respuesta=prompt("Desea ingresar otra venta? si/no").toLowerCase();
+  while (!(respuesta == "si"||respuesta == "no")) {
+    respuesta=prompt("ERROR. Desea ingresar otra venta? si/no").toLowerCase();
+  }
+  switch (tipo) {
+    case "muñeca":
+      contadorMuneca++;
+      break;
+    case "rompecabezas":
+      contadorRompecabezas++;
+      if (precioMasBarato > precio && origen == "nacional") {
+        precioMasBarato = precio
+      }
+      break;
+    case "pelota":
+      contadorPelota++;
+      break;
+  }
+if (origen == "importado") {
+  contadorJuguetes++;
+  sumaPrecioJuguetes = sumaPrecioJuguetes+precio;
+}
+recaudacionTotal=recaudacionTotal+precio;
+console.log(tipo,origen,precio);
+}while (respuesta == "si") 
+if (contadorMuneca > contadorRompecabezas && contadorMuneca > contadorPelota) {
+  jugueteMasVendido = "muñeca";
+}
+else if (contadorPelota > contadorRompecabezas) {
+  jugueteMasVendido = "pelota";
+}
+else{
+  jugueteMasVendido = "rompecabezas";
+}
+alert("jueguete mas vendido ess"+jugueteMasVendido);
+if(contadorJuguetes!=0){
+  promedioJuguetes=sumaPrecioJuguetes/contadorJuguetes;
+  alert(" promedio del precio de los jueguetes importados es "+promedioJuguetes);
+}
+alert("la recaudación total fue de "+recaudacionTotal);
+if (precioMasBarato != 5001) {
+  alert("rompecabezas nacional mas barato vendido es de "+precioMasBarato);
+}
+IVA = recaudacionTotal * 0.21;
+totalDeTodo = IVA + recaudacionTotal;
+alert("Se percibió de IVA "+ IVA + " y sumado es $" + totalDeTodo);
+
 }
--- uno.js
+++ uno.js
@@ -1,3 +1,104 @@
-function mostrar() {
-  alert("Ejercicio 1");
+function mostrar()
+{
+  let nombre;
+  let tipo;
+  let sangre;
+  let edad;
+  let sexo;
+  let contadorReptil=0;
+  let acumuladorReptil=0;
+  let promReptil=0;
+  let edadJoven=0;
+  let flagEdad=1;
+  let contadorMascotas=0;
+  let contadorAves=0;
+  let respuesta;
+  let sexoMascota;
+  let sexoJoven;
+  let tipoJoven;
+  let porcentajeAves;
+  let contadorMamifero;
+  let tipoMasCantidad;
+
+do {
+  nombre=prompt("ingrese nombre (entre 3 y 10 caracteres):");
+  while(nombre.lenght<3||nombre.lenght>10){
+    nombre=prompt("ERROR. ingrese nombre (entre 3 y 10 caracteres):");
+  }
+  tipo=prompt( " ingrese tipo de mascota (ave/mamifero/reptil):").toLowerCase();
+  while(!(tipo=="ave"||tipo=="mamifero"||tipo=="reptil")){
+    tipo=prompt("ERROR. ingrese tipo de mascota (ave/mamifero/reptil):").toLowerCase();
+  }
+  if (tipo=="reptil") {
+    sangre="fria"
+  }
+  else{
+    sangre=prompt("ingrese tipo de sangre (fria/calida):").toLowerCase();
+    while (!(sangre=="fria"||sangre=="calida")) {
+      sangre=prompt("ERROR. ingrese tipo de sangre (fria/calida):").toLowerCase();
+    }
+  }
+  edad=parseInt(prompt("ingrese edad : (mayor a cero):"));
+  while (isNaN(edad)||edad<1) {
+    edad=parseInt(prompt("ERROR. ingrese edad: mayor a cero):"));
+  }
+  sexo=prompt("seleccione su sexo(m para macho/h para hembra):").toLowerCase();
+  while (!(sexo == "m"||sexo == "h")) {
+    sexo=prompt("ERROR. seleccione su sexo(m/h):").toLowerCase();
+  }
+  respuesta=prompt("quiere ingresar otra mascota? si/no").toLowerCase();
+  while (!(respuesta == "si"||respuesta=="no")) {
+    respuesta=prompt("ERROR. Desea ingresar otra mascota? si/no").toLowerCase();
+  }
+  alert(nombre, tipo, sangre, edad, sexo, respuesta);
+switch (tipo) {
+  case "ave":
+    contadorAves++;
+    break;
+  case "reptil":
+    acumuladorReptil=acumuladorReptil+edad;
+    contadorReptil++;
+    break;
+  case "mamifero":
+    contadorMamifero++;
+    break;
 }
+if (sexo=="m") {
+  sexoMascota="macho"
+}
+else{
+  sexoMascota="hembra"
+}
+
+if (flagEdad==1||edadJoven>edad) {
+  edadJoven=edad;
+  sexoJoven=sexo;
+  tipoJoven=tipo;
+  flagEdad=0
+}
+contadorMascotas++;
+
+} while (respuesta == "si");
+
+if (contadorReptil != 0) {
+  promReptil=acumuladorReptil/contadorReptil;
+  alert("El promedio de edad de los reptiles es de "+ promReptil );
+}
+alert("La mascota mas joven es de tipo "+tipoJoven+" de sexo "+sexoMascota+" y tiene "+edadJoven+" años.");
+porcentajeAves=(contadorAves * 100) / contadorMascotas;
+alert("El porcentaje de aves es de "+ porcentajeAves+" %");
+if (contadorMamifero > contadorReptil||contadorMamifero > contadorAves) {
+  tipoMasCantidad= "mamifero";
+}
+else if (contadorReptil  > contadorAves) {
+  tipoMasCantidad  = "reptil";
+} else {
+  tipoMasCantidad = "aves";
+}
+alert("El tipo de mascota que mas cantidad hay es: " + tipoMasCantidad);
+
+}
+
+
+
+
